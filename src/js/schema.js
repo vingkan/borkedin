5b1fd1e6ff2784403d8e969ecff0a17b820b2fbf
Vinesh Kannan <vinesh@omnipointment.com>
2017-10-21 18:40:59 -0500

    Add dog schema

---
+++ src/js/schema.js
@@ -0,0 +1,17 @@
+{
+	name: "Colt Daniels",
+	image: "https://vingkan.github.io/borkedin/public/img/colt-head.jpg",
+	experience: {
+		a: {
+			title: "Emotional Support Dog",
+			range: "January 2017 - Present",
+			description: "I help. hoomans. be happy and content with their lives. because i love them. and want them to be happy."
+		},
+		b: {
+			title: "Rolling Over",
+			range: "Since I was a yung pup",
+			description: "I help. hoomans. be happy and content with their lives. because i love them. and want them to be happy."
+		}
+	}
+
+}
